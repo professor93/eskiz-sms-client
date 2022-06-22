169934b8e97ea0d8527dc3a9e5a6446a50132960
Inoyatulloh <mronx@ya.ru>
2022-06-22 23:19:50 +0500

    update config to defaults

--- config/eskiz-sms-client.php
+++ config/eskiz-sms-client.php
@@ -1,5 +1,8 @@
 <?php
 // config for Uzbek/EskizSmsClient
 return [
-
+    'token_lifetime' => env('ESKIZ_SMS_TOKEN_DURATION', 24 * 3600 * 30),
+    'api_url' => env('ESKIZ_SMS_URL', 'http://notify.eskiz.uz/api/'),
+    'email' => env('ESKIZ_SMS_EMAIL', 'test@eskiz.uz'),
+    'password' => env('ESKIZ_SMS_PASSWORD', 'j6DWtQjjpLDNjWEk74Sx'),
 ];
