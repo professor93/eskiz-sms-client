8ddf637c24d2c28a6f78f81e67d5d937ae6e76c1
Mr.Professor <mronx@ya.ru>
2022-08-30 18:48:09 +0500

    Add proxy settings

--- config/eskiz-sms-client.php
+++ config/eskiz-sms-client.php
@@ -5,4 +5,8 @@ return [
     'api_url' => env('ESKIZ_SMS_URL', 'http://notify.eskiz.uz/api/'),
     'email' => env('ESKIZ_SMS_EMAIL', 'test@eskiz.uz'),
     'password' => env('ESKIZ_SMS_PASSWORD', 'j6DWtQjjpLDNjWEk74Sx'),
+    'proxy_url' => env('ESKIZ_PROXY_URL'),
+    'proxy_proto' => env('ESKIZ_PROXY_PROTO'),
+    'proxy_host' => env('ESKIZ_PROXY_HOST'),
+    'proxy_port' => env('ESKIZ_PROXY_PORT'),
 ];
--- src/EskizSmsClientServiceProvider.php
+++ src/EskizSmsClientServiceProvider.php
@@ -9,19 +9,41 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class EskizSmsClientServiceProvider extends PackageServiceProvider
 {
+    private $config = [];
+
     public function configurePackage(Package $package): void
     {
         $package->name('eskiz-sms-client')->hasConfigFile();
         $this->app->singleton(EskizSmsClient::class, function () {
-            $config = config('eskiz-sms-client');
-            Http::macro('eskiz', fn (): PendingRequest => Http::baseUrl($config['api_url']));
+            $this->config = config('eskiz-sms-client');
+            Http::macro('eskiz', function (): PendingRequest {
+                $options = [];
+                if ($this->hasConfig('proxy_url') && str_contains($this->config['proxy_url'], '://')) {
+                    $options['proxy'] = $this->config['proxy_url'];
+                } elseif ($this->hasConfig('proxy_proto', 'proxy_host', 'proxy_port')) {
+                    $options['proxy'] = $this->config['proxy_proto'] . '://' . $this->config['proxy_host'] . ':' . $this->config['proxy_port'];
+                }
+
+                return Http::baseUrl($this->config['api_url'])->withOptions($options);
+            });
 
             return new EskizSmsClient(
-                email: $config['email'],
-                password: $config['password'],
-                tokenLifetime: $config['token_lifetime'],
-                sender: $config['sender'] ?? '4546',
+                email: $this->config['email'],
+                password: $this->config['password'],
+                tokenLifetime: $this->config['token_lifetime'],
+                sender: $this->config['sender'] ?? '4546',
             );
         });
+    }
+
+    private function hasConfig(...$keys): bool
+    {
+        foreach ($keys as $key) {
+            $val = $this->config[$key] ?? false;
+            if ($val === false || (is_string($val) && strlen($val) === 0) || (is_array($val) && count($val) === 0)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
